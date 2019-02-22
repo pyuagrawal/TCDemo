1fa16080546f52e8b6c638af8a3ac66c28a2a587
Peeyush <pyu.agrawal@gmail.com>
2019-02-22 11:35:28 +1100

    Hello World

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -1,8 +1,10 @@
-﻿using System;
-
-namespace ClassLibrary1
+﻿namespace ClassLibrary1
 {
     public class Class1
     {
+        public string GetName()
+        {
+            return "Hello World!";
+        }
     }
 }
